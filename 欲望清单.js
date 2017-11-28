c68cc9aa330d23ca92cfd1dd346e32421c86a168
Nicktimebreak <shxnick@gmail.com>
2017-11-28 21:09:32 +0800

    细节优化

--- 欲望清单.js
+++ 欲望清单.js
@@ -118,182 +118,326 @@ const searchView = {
 const detailView = {
   type: "view",
   props: {
-    title: "详情页"
+    title: "详情页",
+    //scrollEnabled: true,
+    //contentSize: $size(0, 1000)
   },
   views: [{
-    type: "text",
-    props: {
-      id: "filmName",
-      //text: "Originated in Power Flownhttps://t.me/Flow_Script\nVersion: 1.1",
-      editable: false,
-      textColor: $color("black"),
-      font: $font(15),
-      align: $align.left,
-      //autoFontSize: true,
-      //scrollEnabled: false
+      type: "text",
+      props: {
+        id: "filmName",
+        //text: "Originated in Power Flownhttps://t.me/Flow_Script\nVersion: 1.1",
+        editable: false,
+        textColor: $color("black"),
+        font: $font(15),
+        align: $align.left,
+        //autoFontSize: true,
+        scrollEnabled: false,
+        hidden: false,
+        lines: 1,
+        insets: $insets(0, 0, 0, 0)
+      },
+
+      layout: function(make, view) {
+        make.top.inset(10)
+        make.left.right.inset(5)
+        //make.height.equalTo(70)
+      }
+    }, {
+      type: "image",
+      props: {
+        id: "filmCover",
+        //scale: 2,
+        //src: "https://i.loli.net/2017/11/14/5a0a553e1c420.jpg"
+      },
+      layout: function(make, view) {
+        var width = $device.info.screen.width - 20;
+        var height = width * 67 / 100
+        make.left.inset(10)
+        make.top.equalTo($("filmName").bottom).offset(5)
+        make.size.equalTo($size(width, height))
+      }
+    }, {
+      type: "text",
+      props: {
+        text: "影片信息:",
+        hidden: true,
+        bgcolor: $color("white"),
+        id: "aboutFilm",
+        hidden: true,
+        font: $font("bold", 17),
+        editable: false,
+        textColor: $color("black"),
+        align: $align.left,
+        //autoFontSize: true,
+        scrollEnabled: false,
+        insets: $insets(0, 0, 0, 0)
+      },
+      layout: function(make, view) {
+        make.left.inset(5)
+        make.top.equalTo($("filmCover").bottom).offset(5)
+        //make.height.equalTo(20)
+      },
+
+    }, {
+      type: "text",
+      props: {
+        id: "filmInfo",
+        //text: "时间: 2017-12-04    长度: 124分钟    发行商: SOD",
+        editable: false,
+        textColor: $color("black"),
+        font: $font(12),
+        align: $align.left,
+        scrollEnabled: false,
+        insets: $insets(0, 0, 0, 0)
+      },
+
+      layout: function(make, view) {
+        make.top.equalTo($("aboutFilm").bottom).offset(5)
+        make.left.inset(5)
+        //make.width.equalTo($device.info.screen.width)
+      }
+    }, {
+      type: "text",
+      props: {
+        text: "参演女优:",
+        bgcolor: $color("white"),
+        id: "whoInFilm",
+        font: $font("bold", 17),
+        editable: false,
+        textColor: $color("black"),
+        align: $align.left,
+        //autoFontSize: true,
+        scrollEnabled: false,
+        hidden: true,
+        insets: $insets(0, 0, 0, 0)
+      },
+      layout: function(make, view) {
+        make.left.inset(5)
+        make.top.equalTo($("filmInfo").bottom).offset(5)
+        //make.height.equalTo(20)
+      },
+
+    }, {
+      type: "matrix",
+      props: {
+        id: "filmActress",
+        itemHeight: 100,
+        columns: 4,
+        spacing: 6,
+        square: false,
+        bgcolor: $color("clear"),
+        template: [{
+          type: "view",
+          props: {
+            bgcolor: $color("#ededed")
+          },
+          views: [{
+            type: "image",
+            props: {
+              id: "actressCover",
+            },
+            layout: function(make, view) {
+              make.left.right.top.inset(3);
+              make.height.equalTo(80)
+            }
+          }, {
+            type: "label",
+            props: {
+              id: "actressName",
+              textColor: $color("black"),
+              //text: "dfcvv",
+              align: $align.center,
+              font: $font("bold", 10),
+              autoFontSize: true,
+            },
+            layout: function(make) {
+              make.left.right.inset(0)
+              make.top.equalTo($("actressCover").bottom).offset(2)
+              //make.height.equalTo(20)
+            },
+          }],
+          layout: $layout.fill
+        }],
+      },
+      layout: function(make, view) {
+        make.left.right.inset(5)
+        make.bottom.inset(40)
+        make.top.equalTo($("whoInFilm").bottom).offset(5)
+      },
+      events: {
+        didSelect(sender, indexPath, data) {
+          //$ui.action(data.actressName.text)
+          url = data.link
+          var actress = data.actressName.text
+          var cover = data.actressCover.src
+          actressPage = 0
+          actressView(actress, cover)
+          $("actressView").data = []
+          getActress(url)
+        }
+      }
+
+    }, {
+      type: "button",
+      props: {
+        id: "megnet",
+        bgcolor: $color("#ededed"),
+        radius: 0,
+        title: "搜磁链",
+        titleColor: $color("black"),
+        alpha: 1,
+        radius: 6
+      },
+      layout: function(make, view) {
+        make.left.inset(10)
+        make.bottom.inset(5)
+        make.width.equalTo(view.super).dividedBy(3).offset(-10)
+        make.height.equalTo(30)
+      },
+      events: {
+        tapped(sender) {
+          //$clipboard.text = favCode
+          $safari.open({
+            url: "https://btso.pw/search/" + encodeURI(favCode)
+          })
+        }
+      }
+
+    }, {
+      type: "button",
+      props: {
+        id: "check",
+        bgcolor: $color("#ededed"),
+        radius: 0,
+        title: "查看截图",
+        titleColor:$color("black"),
+        alpha: 1,
+        radius: 6
+      },
+      layout: function(make, view) {
+        make.bottom.inset(5)
+        make.left.equalTo($("megnet").right).offset(5)
+        make.width.equalTo(view.super).dividedBy(3).offset(-10)
+        make.height.equalTo(30)
+      },
+      events: {
+        tapped(sender) {
+          if (screenData == "no") {
+            $ui.toast("☹️ 暂无截图", 1)
+            return
+          } else {
+            //$ui.action(screenData)
+            $ui.push(screenshotView)
+            screenData.map(function(i) {
+              //$ui.action(i.link)
+              $("screenshot").data = $("screenshot").data.concat({
+                screenshotCover: {
+                  src: i.src
+                },
+                link: i.link
+              })
+            })
+          }
+
+        }
+
+      }
+
+    }, {
+      type: "button",
+      props: {
+        id: "favorite",
+        bgcolor: $color("#5e9ced"),
+        title: "收藏",
+        titleColor:$color("white"),
+        alpha: 1,
+        radius: 6
+      },
+      layout: function(make, view) {
+        make.bottom.inset(5)
+        make.right.inset(10)
+        make.width.equalTo(view.super).dividedBy(3).offset(-10)
+        make.height.equalTo(30)
+      },
+      events: {
+        tapped(sender) {
+          var data = {
+            "src": favSrc,
+            "info": favInfo,
+            "link": favLink
+          }
+          if ($("menu").index == 0) {
+            if ($("favorite").title == "收藏") {
+              $("favorite").title = "取消收藏"
+              favoriteButtonTapped("add", data)
+            } else if ($("favorite").title == "取消收藏") {
+              $("favorite").title = "收藏"
+              favoriteButtonTapped("cancel", data)
+            }
+          } else if ($("menu").index == 1) {
+            if ($("favorite").title == "归档") {
+
+              $("favorite").title = "已归档"
+              favoriteButtonTapped("archive", data)
+
+            }
+          } else {
+            if ($("favorite").title == "删除") {
+              $("favorite").title = "已删除"
+              favoriteButtonTapped("del", data)
+            }
+
+          }
+
+        }
+      }
+
     },
 
-    layout: function(make, view) {
-      make.top.inset(10)
-      make.height.equalTo(50)
-      make.width.equalTo($device.info.screen.width)
-    }
-  }, {
-    type: "image",
-    props: {
-      id: "filmCover",
-      //scale: 2,
-      //src: "https://i.loli.net/2017/11/14/5a0a553e1c420.jpg"
-    },
-    layout: function(make, view) {
-      var width = $device.info.screen.width - 20;
-      var height = width * 67 / 100
-      make.left.inset(10)
-      make.top.equalTo($("filmName").bottom).offset(10)
-      make.size.equalTo($size(width, height))
-    }
-  }, {
-    type: "text",
-    props: {
-      id: "filmInfo",
-      //text: "时间: 2017-12-04    长度: 124分钟    发行商: SOD",
-      editable: false,
-      textColor: $color("black"),
-      font: $font("bold", 12),
-      align: $align.center,
-      scrollEnabled: false
-    },
+  ],
+  layout: $layout.fill
 
-    layout: function(make, view) {
-      make.top.equalTo($("filmCover").bottom).offset(5)
-      make.height.equalTo(25)
-      make.width.equalTo($device.info.screen.width)
-    }
-  }, {
+}
+
+const screenshotView = {
+
+  type: "view",
+  props: {
+    title: "影片截屏"
+  },
+  views: [{
     type: "matrix",
     props: {
-      id: "filmActress",
-      itemHeight: 100,
-      columns: 4,
+      id: "screenshot",
+      itemHeight: 130,
+      columns: 3,
       spacing: 1,
       square: false,
       bgcolor: $color("clear"),
       template: [{
         type: "image",
         props: {
-          id: "actressCover",
+          id: "screenshotCover",
+          src: "https://i.loli.net/2017/11/14/5a0a553e1c420.jpg"
         },
-        layout: function(make, view) {
-          make.left.right.top.inset(5);
-          make.height.equalTo(80)
-        }
-      }, {
-        type: "label",
-        props: {
-          id: "actressName",
-          textColor: $color("black"),
-          //text: "dfcvv",
-          align: $align.center,
-          font: $font(10),
-          autoFontSize: true
-        },
-        layout: function(make) {
-          make.left.right.inset(0)
-          make.top.equalTo($("actressCover").bottom).offset(5)
-          //make.height.equalTo(20)
-        },
-      }],
+        layout: $layout.fill
+
+      }, ],
     },
-    layout: function(make, view) {
-      make.left.right.bottom.inset(5)
-      make.top.equalTo($("filmInfo").bottom).offset(5)
-    },
+    layout: $layout.fill,
     events: {
       didSelect(sender, indexPath, data) {
         //$ui.action(data.actressName.text)
         url = data.link
-        var actress = data.actressName.text
-        var cover = data.actressCover.src
-        actressPage = 0
-        actressView(actress, cover)
-        $("actressView").data = []
-        getActress(url)
-      }
-    }
-
-  }, {
-    type: "button",
-    props: {
-      id: "megnet",
-      bgcolor: $color("black"),
-      radius: 0,
-      title: "搜磁链",
-      alpha: 0.7
-    },
-    layout: function(make, view) {
-      make.left.bottom.inset(0)
-      make.width.equalTo(view.super).dividedBy(2).offset(-1)
-      make.height.equalTo(30)
-    },
-    events: {
-      tapped(sender) {
-        $safari.open({
-          url: "https://cn.torrentkitty.tv/search/" + encodeURI(favCode)
+        $quicklook.open({
+          url: url
         })
-      }
-    }
-
-  }, {
-    type: "button",
-    props: {
-      id: "favorite",
-      bgcolor: $color("black"),
-      radius: 0,
-      title: "收藏",
-      alpha: 0.7
-    },
-    layout: function(make, view) {
-      make.bottom.inset(0)
-      make.left.equalTo($("megnet").right).offset(2)
-      make.width.equalTo(view.super).dividedBy(2)
-      make.height.equalTo(30)
-    },
-    events: {
-      tapped(sender) {
-        var data = {
-          "src": favSrc,
-          "info": favInfo,
-          "link": favLink
-        }
-        if ($("menu").index == 0) {
-          if ($("favorite").title == "收藏") {
-            $("favorite").title = "取消收藏"
-            favoriteButtonTapped("add", data)
-          } else if ($("favorite").title == "取消收藏") {
-            $("favorite").title = "收藏"
-            favoriteButtonTapped("cancel", data)
-          }
-        } else if ($("menu").index == 1) {
-          if ($("favorite").title == "归档") {
-
-            $("favorite").title = "已归档"
-            favoriteButtonTapped("archive", data)
-
-          }
-        } else {
-          if ($("favorite").title == "删除") {
-            $("favorite").title = "已删除"
-            favoriteButtonTapped("del", data)
-          }
-
-        }
-
       }
     }
 
   }],
   layout: $layout.fill
-
 }
 
 function actressView(actress, cover) {
@@ -553,9 +697,11 @@ function getDetail(url) {
   $http.request({
     url: url,
     handler: function(resp) {
+      //演员头像
       var actressReg = /<a class="avatar-box"[\s\S]*?<\/a>/g;
       var match = resp.data.match(actressReg)
       if (match) {
+        $("whoInFilm").hidden = false
         match.map(function(i) {
           var name = /<span>(.*?)<\/span>/.exec(i)[1];
           var nameLink = /href="([\s\S]*?)(")/.exec(i)[1];
@@ -572,17 +718,57 @@ function getDetail(url) {
           });
         })
 
+      } else {
+        $("whoInFilm").hidden = true
       }
+      // 影片详情
       var filmCover = /<a class="bigImage" href="(.*?)"/.exec(resp.data)[1];
       $("filmCover").src = filmCover;
       var filmName = /<a class="bigImage" href="(.*?)" title="(.*?)"/.exec(resp.data)[2];
       $("filmName").text = filmName;
-      var filmTime = /<span class="header">发行时间:<\/span>([\s\S]*?)<\/p>/.exec(resp.data)[1];
-      var filmLast = /<span class="header">长度:<\/span>([\s\S]*?)<\/p>/.exec(resp.data)[1];
-      var filmSource = /<p class="header">发行商:[\s\S]*?">(.*?)<\/a>/.exec(resp.data)[1];
-      $("filmInfo").text = "时间: " + filmTime + "    长度: " + filmLast + "    发行商: " + filmSource;
+      var temp = /<span class="header">发行时间:<\/span>([\s\S]*?)<\/p>/.exec(resp.data);
+      if (temp) {
+        var filmTime = temp[1]
+      } else {
+        var filmTime = "????-??-??"
+      }
+      var temp = /<span class="header">长度:<\/span>([\s\S]*?)<\/p>/.exec(resp.data);
+      if (temp) {
+        var filmLast = temp[1]
+      } else {
+        var filmLast = "???分钟"
+      }
+      var temp = /<p class="header">发行商:[\s\S]*?">(.*?)<\/a>/.exec(resp.data);
+      if (temp) {
+        var filmSource = temp[1]
+      } else {
+        var filmSource = "??"
+      }
+      $("filmInfo").text = "上映: " + filmTime + "    长度: " + filmLast + "    发行商: " + filmSource;
+      $("aboutFilm").hidden = false
 
       //$ui.action(filmSource)
+      //影片截图
+      var regScreenshot = /<a class="sample-box" href="(.*?)"[\s\S]*?<img src="(.*?)">/g
+      var match = resp.data.match(regScreenshot)
+
+      if (match) {
+        screenData = []
+        match.map(function(i) {
+          var screenshot = /<a class="sample-box" href="(.*?)"[\s\S]*?<img src="(.*?)">/g.exec(i)[1];
+                            
+          var screenshotCover = /<a class="sample-box" href="(.*?)"[\s\S]*?<img src="(.*?)">/g.exec(i)[2];
+          //$ui.action(screenshotCover)
+          screenData.push({
+            "src": screenshotCover,
+            "link": screenshot
+          })
+
+        })
+
+      } else {
+        screenData = "no"
+      }
 
     }
   })
@@ -689,7 +875,7 @@ function favoriteButtonTapped(mode, data) {
     if ($("menu").index == 1) {
       $("initialView").delete(idx)
       var length = LocalFavList.length;
-      $("input").placeholder = "已收藏 " + length + " 个番号"
+      $("input").placeholder = "已收藏 " + length + " 部影片"
     }
     LocalData.archive.push(data)
     LocalArcList.push(data.link)
@@ -700,7 +886,7 @@ function favoriteButtonTapped(mode, data) {
     if ($("menu").index == 2) {
       $("initialView").delete(idx)
       var length = LocalArcList.length;
-      $("input").placeholder = "已归档 " + length + " 个番号"
+      $("input").placeholder = "已归档 " + length + " 部影片"
     }
   }
   writeCache()
