4efa017ae0811f8ce13331d7be25091f40f4cadf
Nicktimebreak <shxnick@gmail.com>
2018-05-27 13:26:15 +0800

    更新启动方式

--- Airtable-Movie/main.js
+++ Airtable-Movie/main.js
@@ -6,21 +6,31 @@ if (!$cache.get("apiKey")) {
     type: $kbType.default,
     placeholder: "Input Api Key",
     handler: function(text) {
-      $cache.set("apiKey", text)
-      $ui.toast($l10n("LOAD"),10)
-      main()
+      $cache.set("apiKey", text) 
+      $ui.alert($l10n("START"))
     }
   })
 } else {
-  if ($app.env !== $env.safari) $ui.alert($l10n("START"));
-  else {
+  if ($app.env !== $env.safari) {
     $ui.toast($l10n("LOAD"),10)
-    main()
+    let links = $detector.link($clipboard.text)
+    if(links.length){
+      if(!/\/(\d{5,8})\//g.test(links[0])) wrong();
+      else getMovie(links[0]);
+    }else{
+      $ui.toast("",.1)
+      $ui.alert($l10n("START"))
+    }
+  }else {
+    $ui.toast($l10n("LOAD"),10)
+    let item = $safari.items
+    if(!/\/(\d{5,8})\//g.test(item.baseURI)) wrong();
+    else getMovie(item.baseURI);
   }
 }
-function main() {
-  let item = $safari.items
-  let id = /\/(\d{5,8})\//g.exec(item.baseURI)[1]
+
+function getMovie(url) {
+  let id = /\/(\d{5,8})\//g.exec(url)[1]
   $http.get({
     url: apiUrl + id,
     handler: function(resp) {
@@ -49,5 +59,10 @@ function main() {
       airtable.post(content)
     }
   })
+}
 
+function wrong(){
+  $ui.toast("",.1)  
+  $ui.alert($l10n("WRONG"))
+  return
 }
