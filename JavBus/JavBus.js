76402837f1420a8973782923fab906cd758f894b
Nicktimebreak <shxnick@gmail.com>
2019-03-12 00:12:16 +0800

    japonX

--- JavBus/JavBus.js
+++ JavBus/JavBus.js
@@ -4929,7 +4929,6 @@ function urljs(tk, ac, fg2) {
   var aa = function (p, a, c, k, e, d) { e = function (c) { return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) }; if (!''.replace(/^/, String)) { while (c--) { d[e(c)] = k[c] || e(c) } k = [function (e) { return d[e] }]; e = function () { return '\\w+' }; c = 1 }; while (c--) { if (k[c]) { p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]) } } return p }(fg2, ac, ac, tk, 0, {});
   var url = aa.match(/url:\\\'(\S*?)\\\'/)[1];
   url = url.replace(/\'/g, "");
-  $clipboard.text =url
   play(url)
 }
 
