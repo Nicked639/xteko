ea1b93fe25efb5a2756e8deb8f6c10aba3ade704
Nicktimebreak <shxnick@gmail.com>
2019-05-16 16:51:23 +0800

    SafariInjectionTrans.js

---
+++ SafariInjectionTrans.js
@@ -0,0 +1,3 @@
+$safari.inject(`(function () {
+    var script = document.createElement('script');script.type = 'text/javascript';script.src = 'https://iostranslate-158409.appspot.com/js/trns.js?lang=en&x=0.309590818717985';document.body.appendChild(script);
+  })()`)
