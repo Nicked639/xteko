f35670dbbad6ba2351d179928a412efcb22323c6
Nicktimebreak <shxnick@gmail.com>
2020-03-09 19:54:23 +0800

    update

--- 微博热点.js
+++ 微博热点.js
@@ -1,14 +1,17 @@
 const hotSeachApi =
   "https://api.weibo.cn/2/guest/page?gsid=_2AkMtqmJ0f8NhqwJRmPEdxGnjaIx-wwDEieKb9pOvJRMxHRl-wT9kqnAAtRV6Bm0NBHg_Q_-5Rx4sx0moY_1sSSEoN2zx&uid=1009882141998&wm=3333_2001&i=ddd48a6&b=0&from=1084393010&checktoken=745495b139d5d0943c12418acc7a08f8&c=iphone&networktype=wifi&v_p=60&skin=default&s=ffffffff&v_f=1&did=10dc157a640f1c1bd53cbacbad02326f&lang=zh_CN&sflag=1&ft=0&moduleID=pagecard&uicode=10000011&featurecode=10000085&feed_mypage_card_remould_enable=1&luicode=10000003&count=20&extparam=filter_type%3Drealtimehot%26mi_cid%3D100103%26pos%3D0_0%26c_type%3D30%26display_time%3D1526132043&containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&fid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&page=1";
 
-let containerid = {
-  热门: "102803",
-  小时: "102803_ctg1_9999_-_ctg1_9999",
-  昨日: "102803_ctg1_8899_-_ctg1_8899",
-  前日: "102803_ctg1_8799_-_ctg1_8799",
-  周榜: "102803_ctg1_8698"
-};
+const hotWeiboApi =
+  "https://api.weibo.cn/2/statuses/unread_hot_timeline";
 
+//let containerid = {
+//  "热门":"102803",
+//  "小时":"102803_ctg1_9999_-_ctg1_9999_home",
+//  "昨日":"102803_ctg1_8899_-_ctg1_8899",
+//  "前日":"102803_ctg1_8799_-_ctg1_8799",
+//  "周榜":"102803_ctg1_8698_-_ctg1_8698"
+//}
+let containerid=["","102803","102803_ctg1_9999_-_ctg1_9999_home","102803_ctg1_8899_-_ctg1_8899","102803_ctg1_8799_-_ctg1_8799","102803_ctg1_8698_-_ctg1_8698"]
 const template = {
   props: {
     bgcolor: $color("clear")
@@ -19,8 +22,7 @@ const template = {
       props: {
         id: "label",
         bgcolor: $color("clear"),
-        textColor:
-          $device.isDarkMode == true ? $color("white") : $color("black"),
+        textColor:$app.env==$env.app?$color("black"): $device.isDarkMode == true ? $color("white") : $color("black"),
         align: $align.center,
         font: $font(14)
       },
@@ -55,14 +57,53 @@ const template = {
   ]
 };
 
-function weiboList(temp) {
+
+const template2 = {
+  props: {
+      bgcolor: $color("clear")
+    },
+  views: [
+    {
+      type: "label",
+      props: {
+        id: "label",
+        textColor: $app.env==$env.app?$color("black"): $device.isDarkMode == true ? $color("white") : $color("black"),
+        align: $align.left,
+        font: $font(11),
+        lines:2
+      },
+      layout: function(make, view) {
+        make.right.inset(20);
+        make.centerY.equalTo()
+        make.left.inset(60);
+      }
+    },{
+      type: "label",
+      props: {
+        id: "name",
+        textColor: $app.env==$env.app?$color("black"): $device.isDarkMode == true ? $color("white") : $color("black"),
+        align: $align.left,
+        font: $font("bold",13),
+        lines:2,
+        autoFontSize:true
+      },
+      layout: function(make, view) {
+        make.centerY.equalTo()
+        make.left.inset(10);
+        make.width.equalTo(40)
+      }
+    },
+  ]
+};
+
+function weiboList(id,temp,height) {
   return {
     type: "list",
     props: {
-      id: "hotList",
+      id: id,
       template: temp,
       hidden: true,
-      rowHeight: 35,
+      rowHeight: height,
       bgcolor: $color("clear"),
       actions: [
         {
@@ -76,7 +117,7 @@ function weiboList(temp) {
         },
         {
           title: "微博国际",
-          color: $rgb(242, 152, 000), // default to gray
+          color: $rgb(242, 152,0), // default to gray
           handler: function(sender, indexPath) {
             //            console.log(sender.data[indexPath.row].label.info)
             $cache.set("app", "weibointernational");
@@ -91,23 +132,27 @@ function weiboList(temp) {
           title: "墨客",
           color: $rgb(69, 134, 209),
           handler: function(sender, indexPath) {
-            $cache.set("app", "moke");
-
-            let text = /.、([\s\S]*)/g.exec(
-              sender.data[indexPath.row].label.text
-            )[1];
-            $app.openURL("moke:///search/statuses?query=" + encodeURI(text));
+       $cache.set("app","moke")
+            if($("fireList").hidden==false){
+                      $app.openURL("moke:///status?mid="+sender.data[indexPath.row].label.id)
+                    }else{
+                      let text = /.、([\s\S]*)/g.exec(sender.data[indexPath.row].label.text)[1]
+                      $app.openURL("moke:///search/statuses?query="+encodeURI(text))
+                    }
           }
         }
       ]
     },
-    layout: $layout.fill,
+    layout: (make, view) => {
+      make.left.right.bottom.inset(0)
+      make.top.inset(25)
+    },
     events: {
       didSelect: function(sender, indexPath) {
-        let app = $cache.get("app") || "weibo";
+//        let app = $cache.get("app") || "weibo";
 
-        let name = sender.data[indexPath.row].label.infoname;
-        url = "http://s.weibo.com/weibo?q=%23" + name + "%23&Refer=top";
+        let url = sender.data[indexPath.row].label.link;
+        console.log(sender.data[indexPath.row])
         console.log(url);
         openWeb(url);
       },
@@ -119,10 +164,17 @@ function weiboList(temp) {
 $app.close()
       },
       pulled: function(sender) {
-        $("hotList").remove();
-
-        $("weibo").add(weiboList(template));
-        getHotSearch();
+        if($("tab").index==0){
+           $("hotList").data=[]
+          
+//                  $("weibo").add(weiboList("hotList",template));
+                  getHotSearch();
+        }else{
+          $("fireList").data=[]
+          
+          getFire(containerid[$("tab").index])
+        }
+       sender.endRefreshing()
       }
     }
   };
@@ -139,9 +191,9 @@ function getHotSearch() {
         return;
       }
       let hotCards = data.cards[0].card_group;
+           $("hotList").hidden = false;
+            $("fireList").hidden = true;
       $("hotList").data = [];
-
-      $("hotList").hidden = false;
       var temp = [];
       for (let i = 0; i < hotCards.length; i++) {
         let icon = {};
@@ -178,7 +230,7 @@ function getHotSearch() {
           label: {
             text: prefix + hotCards[i].desc,
             info: hotCards[i].scheme,
-            infoname: hotCards[i].desc
+            link:"http://s.weibo.com/weibo?q=%23" + hotCards[i].desc + "%23&Refer=top"
           },
           icon: icon
         });
@@ -188,6 +240,74 @@ function getHotSearch() {
     }
   });
   //  alert($props($("tab")))
+}
+
+function getFire(containerid="102803") {
+  $http.request({
+    method: "POST",
+    url: hotWeiboApi,
+    header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Host: "api.weibo.cn",
+        "User-Agent": "WeiboOverseas/3.7.3 (iPhone; iOS 13.4; Scale/3.00)",
+        "X-Sessionid": "FB2B9D47-FFCD-4A94-8D33-FDE1313557D9"
+      },
+      body: {
+    adss:"a829644381d03fe621933a54999bc051",
+    aid:"01A_gQlePB46dDPjzk7p6P7s8w1dwrmoa-4SYtwkBUm38_q48.",
+    c:"weicoabroad",
+    containerid:containerid,
+    count:"25",
+    extparam:"discover|new_feed",
+    fid:"102803_ctg1_9999_-_ctg1_9999_home",
+    from:"1237393010",
+    fromlog:"1028039999",
+    group_id:"1028039999",
+    gsid:"_2A25zYd3xDeRxGedP71YS8SbFzT2IHXVuN1Y5rDV6PUJbkdAKLW7VkWpNX-gVeSRDdU4PP1TCE6amEC95bgTLekqn",
+    i:"15a1eb5",
+    lang:"zh_CN",
+    refresh:"pulldown",
+    s:"603068d8",
+    since_id:"4480206255699492",
+    trim_level:1,
+    trim_page_recom:0,
+    tz:"Asia/Shanghai",
+    ua:"iPhone10,3_iOS13.4_Weibo_intl._3730_wifi",
+    uid:"1144318961",
+    v_p:59
+    
+      },
+    
+    handler: function(resp) {
+      let data = resp.data;
+//      if (data.errmsg) {
+//        alert(data.errmsg);
+//        return;
+//      }
+//      $clipboard.text=JSON.stringify(data)
+      $("hotList").hidden = true;
+      $("fireList").hidden = false;
+      $("fireList").data = [];
+      if($("tab").index==1)
+        $ui.toast(data.remind_text_old, 1);
+      else
+        $ui.clearToast()
+      let hots = data.statuses;
+      for(let i=0;i<hots.length;i++){
+        $("fireList").data = $("fireList").data.concat({
+          label:{
+            text:hots[i].text,
+            info:hots[i].scheme,
+            id:/.*mblogid=([\s\S]*)/g.exec(hots[i].scheme)[1],
+            link:"https://m.weibo.cn/"+hots[i].user.id+"/"+hots[i].id
+          },
+          name:{
+            text:hots[i].user.name
+          }
+        })
+      }
+    }
+  });
 }
 
 function timeConvert(unixTime) {
@@ -362,7 +482,31 @@ function show() {
       navBarHidden:true,
       //      bgcolor: $color("clear")
     },
-    views: [weiboList(template)]
+    views: [weiboList("hotList",template,35),
+    weiboList("fireList",template2,40),
+          {
+            type: "tab",
+            props: {
+              id:"tab",
+              items: ["热搜", "热门","小时","昨日","前日","周榜"],
+              radius:5
+            },
+            layout: function(make, view) {
+              make.top.inset(0);
+              make.centerX.equalTo();
+              make.height.equalTo(22)
+            },
+            events: {
+              changed: function(sender) {
+                $ui.toast("载入中...",10)
+                if (sender.index == 0) getHotSearch();
+                 else  {
+                  getFire(containerid[sender.index]);
+                }
+              }
+            }
+          }
+    ]
   });
 }
 
